1657c9af94a27da2a7466471603b3748e8f264b3
Maxime Cagigos <maxime.cagigos@enyx.fr>
2019-07-24 15:28:56 +0200

    [test] Increase timeout test error margins
    We were getting a way too many false negatives in certain
    circumstances...

--- tests/example/tests/slowgrep.sh
+++ tests/example/tests/slowgrep.sh
@@ -5,10 +5,10 @@ slowgrep() { sleep "$1"; shift; grep "$@"; }
 regtest_dir=ascii
 
 regtest slow-hello-1 \
-    slowgrep .1 Hello {}/hello.txt
+    slowgrep .2 Hello {}/hello.txt
 
 regtest slow-hello-2 \
-    slowgrep 1 Hello {}/hello.txt
+    slowgrep .3 Hello {}/hello.txt
 
 regtest slow-hello-3 \
     slowgrep 0 Hello {}/hello.txt
