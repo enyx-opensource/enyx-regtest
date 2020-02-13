fdb7f2a7dca2e573cce1e772a82496ab66d2b684
Maxime Cagigos <maxime.cagigos@enyx.fr>
2020-02-13 14:44:52 +0100

    checksum-files: With -g|--generate, check existing checksums
    When 'regtest_generate' is set (e.g. with -g|--generate) and a reference
    file's checksum is already recorded in the checksum file and does not
    correspond to the actual checksum of the file —which presumably has just
    been generated, comment out (with a '#') the invalid lines in the
    checksum file before appending the new checksum to the checksum file and
    returning an error.
    
    TODO: Tests!

--- lib/checksum-files.sh
+++ lib/checksum-files.sh
@@ -14,14 +14,38 @@ regtest_ref_checksum_file=reference-md5sums
 # regtest_ref_checksum <path> (monkey-patch)
 # Get a stored checksum for a file. If it hasn't been computed yet, compute it and store it first.
 regtest_ref_checksum() {
-    local path=$1 filename sum
+    local path=$1 filename sum tmp_checksum_file
 
     filename=$(basename -- "$path")
     sum=$(gawk -v p="$filename" '!/^#/ && $2 == p { print $1; exit; }' \
                "$regtest_ref_checksum_file")
 
     if [[ -n "$sum" ]]; then
-        printf '%s\n' "$sum"
+        if [[ ! "$regtest_generate" ]]; then
+            printf '%s\n' "$sum"
+        else
+            regtest_printn >&2 \
+                'Reference output checksum already recorded in %s. Checking it...' \
+                "$regtest_ref_checksum_file"
+            actual_sum=$(regtest_checksum "$path") || return 1
+            if [[ "$actual_sum" == "$sum" ]]; then
+                regtest_printn >&2 'Checksum OK.'
+                printf '%s\n' "$sum"
+            else
+                regtest_printn >&2 \
+                    'Error: The recorded checksum (%s) differs from the actual checksum (%s)!' \
+                    "$sum" "$actual_sum"
+                regtest_printn >&2 '(Commenting out old checksums...)'
+                tmp_checksum_file=$(mktemp "$_regtest_tmp/checksum-file-$BASHPID-XXXXX")
+                regtest_on_exit "rm -f $(printf %q $tmp_checksum_file)"
+                gawk -v p="$filename" -v s="$actual_sum" \
+                     '$2 == p && $1 != s { print "#" $0; next; } { print }' \
+                     "$regtest_ref_checksum_file" > "$tmp_checksum_file"
+                printf "%s %s\n" "$actual_sum" "$filename" >>"$tmp_checksum_file"
+                mv "$tmp_checksum_file" "$regtest_ref_checksum_file"
+                return 1
+            fi
+        fi
     else
         regtest_printn >&2 'Warning: %s checksum unknown. Computing it...' "$path"
         sum=$(regtest_checksum "$path") || return 1
